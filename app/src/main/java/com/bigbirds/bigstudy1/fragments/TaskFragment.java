db3e4ebd5c18df721f6faacfb52dff32a93ae75b
Linh Le <lequyenlinh110@gmail.com>
2015-12-20 22:53:27 +0700

    Add pager sliding part 2

---
+++ app/src/main/java/com/bigbirds/bigstudy1/fragments/TaskFragment.java
@@ -0,0 +1,26 @@
+package com.bigbirds.bigstudy1.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bigbirds.bigstudy1.R;
+
+/**
+ * Created by Admin on 20/12/2015.
+ */
+public class TaskFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (container == null) {
+            return null;
+        }
+
+        View v = inflater.inflate(R.layout.fragment_task, container, false);
+
+        return v;
+    }
+}
---
+++ app/src/main/java/com/bigbirds/bigstudy1/objects/ItemNote.java
@@ -0,0 +1,20 @@
+package com.bigbirds.bigstudy1.objects;
+
+/**
+ * Created by Admin on 20/12/2015.
+ */
+public class ItemNote {
+    private String content;
+
+    public ItemNote(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
