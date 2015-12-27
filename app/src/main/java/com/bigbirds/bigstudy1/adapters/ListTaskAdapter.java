42d004eab66782e7f542c108d1f5cba4427dfcc0
Linh Le <lequyenlinh110@gmail.com>
2015-12-27 20:36:02 +0700

    Add gradle

--- app/src/main/java/com/bigbirds/bigstudy1/adapters/ListTaskAdapter.java
+++ app/src/main/java/com/bigbirds/bigstudy1/adapters/ListTaskAdapter.java
@@ -1,9 +1,12 @@
 package com.bigbirds.bigstudy1.adapters;
 
 import android.app.Activity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import com.bigbirds.bigstudy1.R;
@@ -50,6 +53,30 @@ public class ListTaskAdapter extends BaseAdapter {
         TextView content = (TextView) convertView.findViewById(R.id.task_content);
 
         content.setText(arrayList.get(position).getContent());
+
+        final ImageView btnArrow = (ImageView) convertView.findViewById(R.id.btn_arrow2);
+        btnArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu taskMenu = new PopupMenu(context, btnArrow);
+                taskMenu.getMenuInflater().inflate(R.menu.note_menu, taskMenu.getMenu());
+                taskMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.note_edit:
+                                return true;
+                            case R.id.note_delete:
+                                return true;
+                            default:
+                                return false;
+                        }
+                    }
+                });
+
+                taskMenu.show();
+            }
+        });
 
         return convertView;
     }
