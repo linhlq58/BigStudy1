85f8662bf72891085ebcb2f86068f3caa3fca5cd
dungntnm58 <dungntm_58@vnu.edu.vn>
2016-01-02 23:30:19 +0700

    Update DatabaseClassHelper.java

--- app/src/main/java/com/bigbirds/bigstudy1/DatabaseClassHelper.java
+++ app/src/main/java/com/bigbirds/bigstudy1/DatabaseClassHelper.java
@@ -1,43 +1,13 @@
 package com.bigbirds.bigstudy1;
 
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.IntentSender;
-import android.content.ServiceConnection;
-import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
-import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.database.Cursor;
-import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.UserHandle;
-import android.support.annotation.Nullable;
-import android.view.Display;
 
 import com.bigbirds.bigstudy1.objects.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Objects;
 
 public final class DatabaseClassHelper extends SQLiteOpenHelper {
 
@@ -174,6 +144,25 @@ public final class DatabaseClassHelper extends SQLiteOpenHelper {
         res.setPhone(cursor.getString(2));
         res.setRoom(cursor.getString(3));
         res.setOtherInfo(cursor.getString(4));
+
+        return res;
+    }
+
+    public ArrayList<Note> getNotesBySubjectID(int id){
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.query("Note", Note.PROPERTIES, "subjectID=?", new String[]{Integer.toString(id)}, null, null, null);
+        cursor.moveToFirst();
+        ArrayList<Note> res = new ArrayList<Note>();
+        while (cursor.isLast()){
+            Note note = new Note();
+            note.setId(cursor.getInt(0));
+            note.setTitle(cursor.getString(1));
+            note.setContent(cursor.getString(2));
+            note.setSubjectID(cursor.getInt(3));
+
+            res.add(note);
+            cursor.moveToNext();
+        }
 
         return res;
     }
