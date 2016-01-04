fc6da2b0ac0c480ed8976db1f4b45c4905c4880f
dungntnm58 <dungntm_58@vnu.edu.vn>
2016-01-04 12:50:38 +0700

    update DatabaseClassHelper.java

--- app/src/main/java/com/bigbirds/bigstudy1/DatabaseClassHelper.java
+++ app/src/main/java/com/bigbirds/bigstudy1/DatabaseClassHelper.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 
 public final class DatabaseClassHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 0;
     public static final String DATABASE_NAME = "local_db.db";
 
     private DatabaseClassHelper(Context context) {
